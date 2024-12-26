b4f10d42b201f1027b2b3517208a2f8124c9405f
Alexey <iskaspb@gmail.com>
2024-12-26 19:31:21 +0800

    started logging example

--- apps/sandbox/logger_example/main.cpp
+++ apps/sandbox/logger_example/main.cpp
@@ -1,7 +1,22 @@
+#include <app/logging.h>
+
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/trivial.hpp>
+
 #include <iostream>
+
+/*
+See notes on log level :
+https://www.boost.org/doc/libs/1_82_0/libs/log/doc/html/log/detailed/expressions.html#log.detailed.expressions.attr.tags
+
+Name scope formatter :
+https://www.boost.org/doc/libs/1_82_0/libs/log/doc/html/log/detailed/expressions.html#log.detailed.expressions.formatters.named_scope
+https://www.boost.org/doc/libs/1_82_0/libs/log/doc/html/log/detailed/attributes.html#log.detailed.attributes.named_scope
+*/
 
 int main()
 {
-    std::cout << "Hello, logger_example!" << std::endl;
-    return 0;
+    // BOOST_LOG_SEV(app::logger::get(), app::logging::trivial::info) << "Logger example output";
+    LOG_INFO("Logger example output with data : " << 10);
+    LOG_INFO(app::logger::get(), "Explicit logger selection with data : " << 30);
 }

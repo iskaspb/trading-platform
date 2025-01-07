fbb41dfa5651c7e1427c170f93dc2406b4ef3c58
Alexey <iskaspb@gmail.com>
2025-01-07 17:53:08 +0800

    refactor configLocator and add more option to initialize application

--- core/app/configLocator.h
+++ core/app/configLocator.h
@@ -24,18 +24,74 @@ struct ConfigLocation
     }
 };
 
+namespace impl
+{
+using std::filesystem::path;
+namespace po = boost::program_options;
+static constexpr char envConfigPath[] = "APP_CONFIG";
+static constexpr char envConfigDir[] = "APP_CONFIG_DIR";
+
+inline bool locateConfigImpl(const path &appPath, ConfigLocation &loc)
+{
+    {
+        //...check if ENV is present and use it as a config file path
+        const auto configFile = std::getenv(envConfigPath);
+        if (configFile)
+        {
+            if (!std::filesystem::exists(configFile))
+            {
+                std::cout << "Using " << envConfigPath << " env variable, ERROR: Config file " << configFile
+                          << " is not accessible" << std::endl;
+                ::exit(1);
+            }
+            loc.locType = ConfigLocation::ENV_CONFIG;
+            loc.path = configFile;
+            return true;
+        }
+    }
+
+    const std::string appConfigName = loc.appName + ".json";
+    {
+        //...check the same directory as the executable ("collocated" config file)
+        const path appDir = appPath.parent_path();
+        const path configFile = appDir / appConfigName;
+        if (std::filesystem::exists(configFile))
+        {
+            loc.locType = ConfigLocation::COLLOCATED;
+            loc.path = configFile.string();
+            return true;
+        }
+    }
+
+    {
+        //...check if ENV is present and use it as a location for config file
+        const auto configDir = std::getenv(envConfigDir);
+        if (configDir)
+        {
+            const path configFile = path(configDir) / appConfigName;
+            if (std::filesystem::exists(configFile))
+            {
+                loc.locType = ConfigLocation::ENV_CONFIG_DIR;
+                loc.path = configFile.string();
+                return true;
+            }
+        }
+    }
+    return false;
+}
+} // namespace impl
+
 inline ConfigLocation locateConfig(int argc, char *argv[], const std::string &overrideAppName = {})
 {
-    using std::filesystem::path;
-    const path appName = path(argv[0]);
-    ConfigLocation result;
-    result.appName = overrideAppName.empty() ? appName.filename().string() : overrideAppName;
+    using namespace impl;
+
+    const path appPath = path(argv[0]);
+    ConfigLocation loc;
+    loc.appName = overrideAppName.empty() ? appPath.filename().string() : overrideAppName;
     {
         //...check if --config is present and use it as a config file path
-        namespace po = boost::program_options;
-
         po::variables_map vm;
-        po::options_description desc{appName.filename().string() + " options"};
+        po::options_description desc{appPath.filename().string() + " options"};
         desc.add_options()("help,h", "Print help")("config,c", po::value<std::string>(), "Config file");
 
         po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -56,61 +112,34 @@ inline ConfigLocation locateConfig(int argc, char *argv[], const std::string &ov
                           << " is not accessible" << std::endl;
                 ::exit(1);
             }
-            result.locType = ConfigLocation::CMD_LINE_CONFIG;
-            result.path = configFile;
-            return result;
+            loc.locType = ConfigLocation::CMD_LINE_CONFIG;
+            loc.path = configFile;
+            return loc;
         }
     }
 
-    static constexpr char envConfigPath[] = "APP_CONFIG";
-    {
-        //...check if ENV is present and use it as a config file path
-        const auto configFile = std::getenv(envConfigPath);
-        if (configFile)
-        {
-            if (!std::filesystem::exists(configFile))
-            {
-                std::cout << "Using " << envConfigPath << " env variable, ERROR: Config file " << configFile
-                          << " is not accessible" << std::endl;
-                ::exit(1);
-            }
-            result.locType = ConfigLocation::ENV_CONFIG;
-            result.path = configFile;
-            return result;
-        }
-    }
-
-    const std::string appConfigName = result.appName + ".json";
-    {
-        //...check the same directory as the executable ("collocated" config file)
-        const path appDir = appName.parent_path();
-        const path configFile = appDir / appConfigName;
-        if (std::filesystem::exists(configFile))
-        {
-            result.locType = ConfigLocation::COLLOCATED;
-            result.path = configFile.string();
-            return result;
-        }
-    }
-
-    static constexpr char envConfigDir[] = "APP_CONFIG_DIR";
-    {
-        //...check if ENV is present and use it as a location for config file
-        const auto configDir = std::getenv(envConfigDir);
-        if (configDir)
-        {
-            const path configFile = path(configDir) / appConfigName;
-            if (std::filesystem::exists(configFile))
-            {
-                result.locType = ConfigLocation::ENV_CONFIG_DIR;
-                result.path = configFile.string();
-                return result;
-            }
-        }
-    }
+    if (locateConfigImpl(appPath, loc))
+        return loc;
 
     std::cout << "Config file not found in the following locations : --config, $" << envConfigPath << ", "
-              << appName.parent_path() << ", $" << envConfigDir << '/' << appConfigName << std::endl;
+              << appPath.parent_path() << ", $" << envConfigDir << '/' << loc.appName << ".json" << std::endl;
     ::exit(1);
 }
+
+inline ConfigLocation locateConfig(const std::string &exePath, const std::string &overrideAppName = {})
+{
+    using namespace impl;
+
+    const path appPath = path(exePath);
+    ConfigLocation loc;
+    loc.appName = overrideAppName.empty() ? appPath.filename().string() : overrideAppName;
+
+    if (locateConfigImpl(appPath, loc))
+        return loc;
+
+    std::cout << "Config file not found in the following locations : $" << envConfigPath << ", "
+              << appPath.parent_path() << ", $" << envConfigDir << '/' << loc.appName << ".json" << std::endl;
+    ::exit(1);
+}
+
 } // namespace app

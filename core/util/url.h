4c476da5b9518aa46c993bb6d7f9d8304f6a2b52
Alexey <iskaspb@gmail.com>
2025-01-25 04:11:41 +0800

    request Binance symbols

--- core/util/url.h
+++ core/util/url.h
@@ -6,11 +6,29 @@
 // URL is a wrapper around boost::urls::url that enforces the use of HTTPS or WSS schemes.
 struct URL : boost::urls::url
 {
-    explicit URL(const std::string &url) : boost::urls::url(url)
+    URL(const std::string &url) : boost::urls::url(url)
     {
-        if (scheme() != "https" && scheme() != "wss")
+        if (scheme().empty())
+            throw std::logic_error("No scheme (wss or https) in URL: " + url);
+        if (!(scheme() == "https" || scheme() == "wss"))
             throw std::logic_error("Unsupported scheme: " + std::string(scheme()));
         if (port().empty())
             set_port("443");
+
+        if (path().empty())
+            set_path("/");
+
+        target_ = path();
+        if (!query().empty())
+            target_ += "?" + query();
+        if (!fragment().empty())
+            target_ += "#" + fragment();
     }
+    const std::string &target() const
+    {
+        return target_;
+    }
+
+  private:
+    std::string target_;
 };

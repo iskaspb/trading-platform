d84b3ecc34961cdb32a7fbc48034a1e174398c8b
Alexey <iskaspb@gmail.com>
2025-01-30 13:13:25 +0800

    example of assembly dependency managments

--- apps/md/binance_symbol_provider/FileSymbolStore.h
+++ apps/md/binance_symbol_provider/FileSymbolStore.h
@@ -2,17 +2,18 @@
 #include "SymbolProviderConfig.h"
 
 #include <app/logging.h>
+#include <assembly/assembly.h>
 
 template <typename Traits> struct FileSymbolStore
 {
     template <HasSymbolProviderConfig Config> explicit FileSymbolStore(Config &config)
     {
-        LOG_DEBUG("FileSymbolStore will store data into " << config.symbolProvider.store);
+        LOG_DEBUG(core::ctx(this).log(), "FileSymbolStore will store data into " << config.symbolProvider.store);
     }
 
     void onResponse(const SymbolData &symbols)
     {
-        LOG_DEBUG("FileSymbolStore stores " << symbols.data.substr(0, 100) << "...");
+        LOG_DEBUG(core::ctx(this).log(), "FileSymbolStore stores " << symbols.data.substr(0, 100) << "...");
         //...add code to store symbols into a file
     }
 };
